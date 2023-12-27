9473fd476275961e3334f0d252a45b0832da309d
Abdulmajeed-Tawfiq <abdulmajeedtawfiq98@gmail.com>
2023-12-27 09:10:37 +0300

    some changes

--- src/pages/main/About.jsx
+++ src/pages/main/About.jsx
@@ -92,39 +92,12 @@ const About = () => {
         </div>
       </div>
 
-      <div
-        className={`skills tn:grid-cols-2 grid sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 justify-items-center	`}
-      >
-        {/* add skills to the about page using skills array and map function */}
+      <div className="skills">
+        {/* /add skills to the about page using skills array and map function */}
         {skills.map((skill) => (
           <div
             ref={ref3}
-            className={`
-            animatet ${inView3 ? "show" : ""}
-            w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-40 xl:h-40
-            skill tn:border-[6px] md:border-[8px] lg:border-[10px] border-red-500 rounded-[50%] border-hidden hover:border-solid`}
-            key={skill.name}
-          >
-            <img
-              src={skill.image}
-              alt={skill.name}
-              className="w-14 h-14 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24"
-            />
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-};
-
-export default About;
-
-{
-  /* <div className="skills">
-        // add skills to the about page using skills array and map function
-        {skills.map((skill) => (
-          <div
-            className="skill"
+            className={`animatet ${inView3 ? "show" : ""} skill`}
             key={skill.name}
             style={{
               backgroundImage: `linear-gradient(to right, #00c0c4 0% ${skill.ratio}%, #efefef ${skill.ratio}% 100%)`,
@@ -134,8 +107,13 @@ export default About;
             <p>{skill.ratio}%</p>
           </div>
         ))}
-      </div> */
-}
+      </div>
+    </section>
+  );
+};
+
+export default About;
+
 {
   /* <div className="skills">
         {skills.map((skill) => (
@@ -161,4 +139,28 @@ export default About;
           </div>
         ))}
       </div> */
+}
+
+{
+  /* <div
+className={`skills tn:grid-cols-2 grid sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 justify-items-center	`}
+>
+ //add skills to the about page using skills array and map function 
+  {skills.map((skill) => (
+  <div
+    ref={ref3}
+    className={`
+    animatet ${inView3 ? "show" : ""}
+    w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 xl:w-40 xl:h-40
+    skill tn:border-[6px] md:border-[8px] lg:border-[10px] border-red-500 rounded-[50%] border-hidden hover:border-solid`}
+    key={skill.name}
+  >
+    <img
+      src={skill.image}
+      alt={skill.name}
+      className="w-14 h-14 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24"
+    />
+  </div>
+))}
+</div> */
 }
