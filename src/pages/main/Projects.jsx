625c8e9b28908c6ac01599c8e514dcb1da5eb235
Abdulmajeed-Tawfiq <abdulmajeedtawfiq98@gmail.com>
2024-03-22 23:35:45 +0300

    edit some styles on projects section

--- src/pages/main/Projects.jsx
+++ src/pages/main/Projects.jsx
@@ -41,17 +41,38 @@ export default function Projects() {
                 scale: 1,
                 speed: 450,
               }}
-              className="project xs:w-[250px] bg-[#1c1f2f] h-fit rounded-2xl"
+              className="project xs:w-[250px] bg-[#1c1f2f] h-fit rounded-2xl min-h-[500px]"
             >
               <div className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
-                <div className="bg-tertiary rounded-[20px] py-5 px-8 min-h-[280px] flex justify-evenly items-center flex-col">
+                <div className="bg-tertiary rounded-[20px] py-4 px-4 min-h-[280px] flex justify-evenly items-center flex-col">
                   <div className="relative w-full h-[230px]">
                     <img
                       src={project.description.split("(screenshot):")[1]}
                       alt="project-image"
                       className="w-full h-full object-cover rounded-2xl"
                     />
-                    <div className="links"></div>
+                    <div className="absolute inset-0 flex justify-between m-2 card-img_hover">
+                      <div
+                        onClick={() => window.open(project.html_url, "_blank")}
+                        className="black-gradient w-8 h-8 rounded-full flex justify-center items-center cursor-pointer"
+                      >
+                        <img
+                          src="../../../public/images/icons/github.svg"
+                          alt="source code"
+                          className="w-full h-full object-contain hover:scale-150"
+                        />
+                      </div>
+                      <div
+                        onClick={() => window.open(project.homepage, "_blank")}
+                        className="black-gradient w-8 h-8 rounded-full flex justify-center items-center cursor-pointer"
+                      >
+                        <img
+                          src="../../../public/images/icons/link6.svg"
+                          alt="source code"
+                          className="w-full h-full object-contain hover:scale-150"
+                        />
+                      </div>
+                    </div>
                   </div>
 
                   <div className="text text-white text-center">
@@ -61,7 +82,7 @@ export default function Projects() {
                     <p className="text-gray-400">
                       {project.description.split("(screenshot):")[0]}
                     </p>
-                    <div className="techs">
+                    <div className="techs flex">
                       {project.topics.map((tag, index) => (
                         <p key={index} className={`text-[14px] ${tag.color}`}>
                           #{tag}
