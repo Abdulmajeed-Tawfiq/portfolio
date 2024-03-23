6ece48b2d4bb69a46f4a5604b7081e99240da4ba
Abdulmajeed-Tawfiq <abdulmajeedtawfiq98@gmail.com>
2024-03-23 15:46:42 +0300

    edit some styles on projects section

--- src/pages/main/Projects.jsx
+++ src/pages/main/Projects.jsx
@@ -34,6 +34,7 @@ export default function Projects() {
           <motion.div
             key={index}
             variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+            className="relative"
           >
             <Tilt
               options={{
@@ -41,54 +42,50 @@ export default function Projects() {
                 scale: 1,
                 speed: 450,
               }}
-              className="project xs:w-[250px] bg-[#1c1f2f] h-fit rounded-2xl min-h-[500px]"
+              className="xs:w-[250px] bg-[#1c1f2f] rounded-2xl min-h-[500px]"
             >
-              <div className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
-                <div className="bg-tertiary rounded-[20px] py-4 px-4 min-h-[280px] flex justify-evenly items-center flex-col">
-                  <div className="relative w-full h-[230px]">
-                    <img
-                      src={project.description.split("(screenshot):")[1]}
-                      alt="project-image"
-                      className="w-full h-full object-cover rounded-2xl"
-                    />
-                    <div className="absolute inset-0 flex justify-between m-2 card-img_hover">
-                      <div
-                        onClick={() => window.open(project.html_url, "_blank")}
-                        className="black-gradient w-8 h-8 rounded-full flex justify-center items-center cursor-pointer"
-                      >
-                        <img
-                          src="../../../public/images/icons/github.svg"
-                          alt="source code"
-                          className="w-full h-full object-contain hover:scale-150"
-                        />
-                      </div>
-                      <div
-                        onClick={() => window.open(project.homepage, "_blank")}
-                        className="black-gradient w-8 h-8 rounded-full flex justify-center items-center cursor-pointer"
-                      >
-                        <img
-                          src="../../../public/images/icons/link.svg"
-                          alt="source code"
-                          className="w-full h-full object-contain hover:scale-150"
-                        />
-                      </div>
+              <div className="  w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+                <div className="relative w-full h ">
+                  <img
+                    src={project.description.split("(screenshot):")[1]}
+                    alt="project-image"
+                    className="w-full h-[230px] object-cover rounded-2xl"
+                  />
+                  <div className="absolute inset-0 flex justify-between m-2 card-img_hover">
+                    <div
+                      onClick={() => window.open(project.html_url, "_blank")}
+                      className="black-gradient w-8 h-8 rounded-full flex justify-center items-center cursor-pointer"
+                    >
+                      <img
+                        src="../../../public/images/icons/github.svg"
+                        alt="source code"
+                        className="w-full h-full object-contain hover:scale-150"
+                      />
+                    </div>
+                    <div
+                      onClick={() => window.open(project.homepage, "_blank")}
+                      className="black-gradient w-8 h-8 rounded-full flex justify-center items-center cursor-pointer"
+                    >
+                      <img
+                        src="../../../public/images/icons/link.svg"
+                        alt="source code"
+                        className="w-full h-full object-contain hover:scale-150"
+                      />
                     </div>
                   </div>
+                </div>
 
-                  <div className="text text-white text-center">
-                    <h3 className="text-[20px] font-bold my-4">
-                      {project.name}
-                    </h3>
-                    <p className="text-gray-400">
-                      {project.description.split("(screenshot):")[0]}
-                    </p>
-                    <div className="techs flex">
-                      {project.topics.map((tag, index) => (
-                        <p key={index} className={`text-[14px] ${tag.color}`}>
-                          #{tag}
-                        </p>
-                      ))}
-                    </div>
+                <div className="text text-white text-center">
+                  <h3 className="text-[20px] font-bold my-4">{project.name}</h3>
+                  <p className="text-gray-400 px-5">
+                    {project.description.split("(screenshot):")[0]}
+                  </p>
+                  <div className="flex absolute bottom-6 left-6 gap-3 ">
+                    {project.topics.map((tag, index) => (
+                      <p key={index} className={`text-[14px] text-[#00c5c3]`}>
+                        #{tag}
+                      </p>
+                    ))}
                   </div>
                 </div>
               </div>
